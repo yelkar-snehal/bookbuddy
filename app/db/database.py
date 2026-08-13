da5a42b6c46facac844793e20648e7dec4c453ec
yelkar-snehal <yelkarsnehal9@gmail.com>
2026-08-14 01:19:34 +0530

    init db

---
+++ app/db/database.py
@@ -0,0 +1,15 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+DATABASE_URL = os.environ["DATABASE_URL"]
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+)
